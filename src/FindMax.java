c29b9ba3df6bd8512dbde1aa25730349237f79a3
dustin <dustin@dustin-PC>
2017-10-08 11:36:42 -0700

    Adding my first mod...

--- src/FindMax.java
+++ src/FindMax.java
@@ -5,7 +5,9 @@ public class FindMax {
 	 * Define a program that finds the maximum number when 2 numbers are given.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if( 2<3) {
+			System.out.println(true);
+		}
 
 	}
 
